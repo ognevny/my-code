c780f3400f990a1c8330a8c22569e2a1e1b8cffd
ognevnydemon <maksapple2306@gmail.com>
2023-04-07 13:11:19 +0300

    gigaimprove of longman.cpp

--- dad-is-great-in-C/longman.cpp
+++ dad-is-great-in-C/longman.cpp
@@ -4,24 +4,19 @@ reference to "Long, long, maaaaaaaan" (Japanese ad) */
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 int main() {
     string line;
     getline(cin, line);
-    size_t end = line.find_last_not_of(" ");
-    string trimmed = line.substr(0, end + 1);
-    vector<string> slices;
-
-    while (!trimmed.empty()) {
-        size_t slice = trimmed.find_first_of(" ");
-        slices.push_back(trimmed.substr(0, slice));
-        if (slice == string::npos) break;
-        trimmed = trimmed.substr(slice + 1); }
-
+    stringstream ss(line);
+    string word;
     int max = 0;
     string maxstr;
-    for (int i = 0; i < slices.size(); i++) {
-        if (slices[i].size() > max) { maxstr = slices[i]; max = slices[i].size(); } }
+    while (ss >> word) {
+        if (word.size() > max) {
+            maxstr = word;
+            max = word.size(); } }
     cout << maxstr << endl << max;
     return 0; }
