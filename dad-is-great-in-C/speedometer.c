9655d4b48ee468a395d8902bfe9748027c50cd86
Maksim Bondarenkov <119937608+ognevnydemon@users.noreply.github.com>
2023-04-08 09:29:12 +0300

    edit speedometer.c to fit speedometer's description

--- dad-is-great-in-C/speedometer.c
+++ dad-is-great-in-C/speedometer.c
@@ -2,8 +2,12 @@
 
 #include <stdio.h>
 
-int main() {
+int s() {
     int n = 1;
     while (n < 1000000000) n++;
+    return n; }
+
+int main() {
+    int n = s();
     printf("%d", n);
     return 0; }
