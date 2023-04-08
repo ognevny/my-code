609fe3f02edcb0dc6f68d026bd39356d4f232bbd
ognevnydemon <maksapple2306@gmail.com>
2023-04-08 18:38:20 +0300

    sys.rs: add translate into DEC

--- rusted/ognlib/src/num_operation/sys.rs
+++ rusted/ognlib/src/num_operation/sys.rs
@@ -15,3 +15,12 @@ pub fn num_sys_hex(mut n: u64, k: u8) -> String {
         res.push(SYS[(n % (k as u64)) as usize]);
         n /= k as u64; }
     res.chars().rev().collect::<String>() }
+
+pub fn num_sys_dec(mut n: u64, k: u8) -> u64 { // translate into DEC number from k system (2 <= k <= 10)
+    let mut dec: u64 = 0;
+    let mut count = 0;
+    while n != 0 {
+        dec += (n % 10) * super::power::bin_pow(k as f64, count) as u64;
+        n /= 10; 
+        count += 1; }
+    dec }
