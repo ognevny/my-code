4a29ca0adb37215559060fca64861279777d7669
ognevnydemon <maksapple2306@gmail.com>
2023-04-08 15:33:49 +0300

    gigaoptimization for num_sys in Rust

--- rusted/ognlib/src/lib.rs
+++ rusted/ognlib/src/lib.rs
@@ -2,55 +2,22 @@
 
 pub mod num_operation {
     pub mod num_sys {
-        use std::collections::HashMap;
+        const SYS: [char; 16] = [
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+        ];
 
         pub fn num_sys(mut n: u64, k: u8) -> u64 {
-            let sys = HashMap::from([
-                (0, '0'),
-                (1, '1'),
-                (2, '2'),
-                (3, '3'),
-                (4, '4'),
-                (5, '5'),
-                (6, '6'),
-                (7, '7'),
-                (8, '8'),
-                (9, '9'),
-                (10, 'A'),
-                (11, 'B'),
-                (12, 'C'),
-                (13, 'D'),
-                (14, 'E'),
-                (15, 'F'),
-            ]);
             let mut res = String::new();
             while n != 0 {
-                res.push(sys[&((n % (k as u64)) as u8)]);
+                res.push(SYS[(n % (k as u64)) as usize]);
                 n /= k as u64; }
             res.chars().rev().collect::<String>().parse().unwrap() }
 
         pub fn num_sys_hex(mut n: u64, k: u8) -> String {
-            let sys = HashMap::from([
-                (0, '0'),
-                (1, '1'),
-                (2, '2'),
-                (3, '3'),
-                (4, '4'),
-                (5, '5'),
-                (6, '6'),
-                (7, '7'),
-                (8, '8'),
-                (9, '9'),
-                (10, 'A'),
-                (11, 'B'),
-                (12, 'C'),
-                (13, 'D'),
-                (14, 'E'),
-                (15, 'F'),
-            ]);
             let mut res = String::new();
             while n != 0 {
-                res.push(sys[&((n % (k as u64)) as u8)]);
+                res.push(SYS[((n % (k as u64)) as usize)]);
                 n /= k as u64; }
             res.chars().rev().collect::<String>() } }
 
