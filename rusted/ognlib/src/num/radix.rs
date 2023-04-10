f0c0541e7c2b13a198503df6def03acf266de07a
ognevnydemon <maksapple2306@gmail.com>
2023-04-10 11:26:32 +0300

    radix: make struct and impl

--- rusted/ognlib/src/num/radix.rs
+++ rusted/ognlib/src/num/radix.rs
@@ -1,58 +1,102 @@
-// TODO: write struct Radix and impl Radix
+// TODO: write ariphmetic functions and make existing functions more useful
 
-const RADIX: [char; 16] = [
+pub const RADIX: [char; 16] = [
     '0', '1', '2', '3', '4', '5', '6', '7',
     '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
-
-/// Translate int number from DEC to radix (2 <= k <= 10)
-/// # Examples
-/// 
-/// ```
-/// use ognlib::num::radix::from_int_to_radix;
-///
-/// assert_eq!(from_int_to_radix(123, 8), 173);
-/// assert_eq!(from_int_to_radix(176, 2), 10110000)
-/// ```
-pub fn from_int_to_radix(mut n: u64, k: u8) -> u64 {
-    let mut res = String::new();
-    while n != 0 {
-        res.push(RADIX[(n % (k as u64)) as usize]);
-        n /= k as u64; }
-    res.chars().rev().collect::<String>().parse().unwrap() }
+pub struct Radix {
+    pub number: usize,
+    pub radix: u8, }
 
 
-/// Translate int number from DEC to radix string (2 <= k <= 16)
-/// # Examples
-/// 
-/// ```
-/// use ognlib::num::radix::from_int_to_radix_string;
-///
-/// assert_eq!(from_int_to_radix_string(123, 16), "7B");
-/// assert_eq!(from_int_to_radix_string(176, 2), "10110000")
-/// ```
-pub fn from_int_to_radix_string(mut n: u64, k: u8) -> String {
-    let mut res = String::new();
-    while n != 0 {
-        res.push(RADIX[(n % (k as u64)) as usize]);
-        n /= k as u64; }
-    res.chars().rev().collect::<String>() }
+impl Radix {
+    /// Translate int number from DEC to radix (2 <= k <= 10)
+    /// # Examples
+    /// 
+    /// ```
+    /// use ognlib::num::radix::Radix;
+    /// 
+    /// let mut n1 = Radix {
+    ///     number: 123,
+    ///     radix: 10, };
+    /// let new1 = n1.from_int_to_radix(8);
+    /// 
+    /// let mut n2 = Radix {
+    ///     number: 176,
+    ///     radix: 10, };
+    /// let new2 = n2.from_int_to_radix(2);
+    /// 
+    /// assert_eq!(new1.number, 173);
+    /// assert_eq!(new1.radix, 8);
+    /// 
+    /// assert_eq!(new2.number, 10110000);
+    /// assert_eq!(new2.radix, 2)
+    /// ```
+    pub fn from_int_to_radix(&mut self, k: u8) -> Radix {
+        let mut res = String::new();
+        while self.number != 0 {
+            res.push(RADIX[(self.number % (k as usize)) as usize]);
+            self.number /= k as usize; }
+        let res = Radix {
+            number: res.chars().rev().collect::<String>().parse().unwrap(),
+            radix: k, };
+        res }
 
 
-/// Translate int number from radix to int DEC number (2 <= k <= 10)
-/// # Examples
-/// 
-/// ```
-/// use ognlib::num::radix::from_radix_to_dec;
-///
-/// assert_eq!(from_radix_to_dec(10110000, 2), 176);
-/// assert_eq!(from_radix_to_dec(444, 10), 444)
-/// ```
-pub fn from_radix_to_dec(mut n: u64, k: u8) -> u64 {
-    let mut dec: u64 = 0;
-    let mut count = 0;
-    while n != 0 {
-        dec += (n % 10) * super::power::bin_pow(k as f64, count) as u64;
-        n /= 10; 
-        count += 1; }
-    dec }
+    /// Translate int number from DEC to radix string (2 <= k <= 16)
+    /// # Examples
+    /// 
+    /// ```
+    /// use ognlib::num::radix::Radix;
+    /// 
+    /// let mut n1 = Radix {
+    ///     number: 123,
+    ///     radix: 10, };
+    /// 
+    /// let mut n2 = Radix {
+    ///     number: 176,
+    ///     radix: 10, };
+    ///
+    /// assert_eq!(n1.from_int_to_radix_string(16), "7B");
+    /// assert_eq!(n2.from_int_to_radix_string(2), "10110000")
+    /// ```
+    pub fn from_int_to_radix_string(&mut self, k: u8) -> String {
+        let mut res = String::new();
+        while self.number != 0 {
+            res.push(RADIX[(self.number % (k as usize)) as usize]);
+            self.number /= k as usize; }
+        res.chars().rev().collect::<String>() }
+
+
+    /// Translate int number from radix to int DEC number (2 <= k <= 10)
+    /// # Examples
+    /// 
+    /// ```
+    /// use ognlib::num::radix::Radix;
+    /// 
+    /// let mut n1 = Radix {
+    ///     number: 10110000,
+    ///     radix: 2, };
+    /// let new1 = n1.from_radix_to_dec();
+    /// 
+    /// let mut n2 = Radix {
+    ///     number: 444,
+    ///     radix: 10, };
+    /// let new2 = n2.from_radix_to_dec();
+    ///
+    /// assert_eq!(new1.number, 176);
+    /// assert_eq!(new1.radix, 10);
+    /// 
+    /// assert_eq!(new2.number, 444);
+    /// assert_eq!(new2.radix, 10);
+    /// ```
+    pub fn from_radix_to_dec(&mut self) -> Radix {
+        let mut dec = Radix {
+            number: 0,
+            radix: 10, };
+        let mut count = 0;
+        while self.number != 0 {
+            dec.number += (self.number % 10) * super::power::bin_pow(self.radix as f64, count) as usize;
+            self.number /= 10; 
+            count += 1; }
+        dec } }
