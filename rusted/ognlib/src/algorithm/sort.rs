4d3569471111ff2f8e49eec714772bdab6c8edc1
ognevnydemon <maksapple2306@gmail.com>
2023-04-27 11:50:08 +0300

    insertion sort algorithm

--- rusted/ognlib/src/algorithm/sort.rs
+++ rusted/ognlib/src/algorithm/sort.rs
@@ -1,4 +1,4 @@
-// TODO: write a code for insertion sort, merge sort and some more
+// TODO: write a code for merge sort and some more
 
 /// Bubble sort algorithm
 /// # Examples
@@ -36,3 +36,24 @@ pub fn search<T: PartialOrd>(arr: &mut [T]) {
         for j in i + 1..n {
             if arr[j] < arr[i_min] { i_min = j } }
         if i_min != i { arr.swap(i, i_min) } } }
+
+
+/// Insertion sort algorithm
+/// # Examples
+///
+/// ```
+/// use ognlib::algorithm::sort::insertion;
+///
+/// let mut arr = vec![5, 3, 4, 1, 2];
+/// insertion(&mut arr);
+/// assert_eq!(arr, [1, 2, 3, 4, 5]);
+/// ```
+
+pub fn insertion<T: Ord>(arr: &mut [T]) {
+    for i in 1..arr.len() {
+        let key = &arr[i];
+        let mut j = i;
+        let pos = arr[..i].binary_search(&key).unwrap_or_else(|pos| pos);
+        while j > pos {
+            arr.swap(j - 1, j);
+            j -= 1; } } }
