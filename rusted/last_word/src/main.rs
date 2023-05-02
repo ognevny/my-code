d08371ff433c8daae77b4696709e334107d12af2
ognevnydemon <maksapple2306@gmail.com>
2023-05-02 18:18:38 +0300

    more clippy

--- rusted/last_word/src/main.rs
+++ rusted/last_word/src/main.rs
@@ -5,7 +5,7 @@ use std::io::stdin;
 fn main() {
     let mut line = String::new();
     stdin().read_line(&mut line).unwrap();
-    let last_space = line.trim().rfind(" ").unwrap_or(0);
+    let last_space = line.trim().rfind(' ').unwrap_or(0);
     let last_word = line[last_space + 1..].trim();
     print!("{last_word}");
 }
