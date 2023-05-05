9c109f5b35c34f17313a4d807417b0e21efb3a98
ognevnydemon <maksapple2306@gmail.com>
2023-05-05 13:08:10 +0300

    format string in mcko.py normally

--- pie/mcko.py
+++ pie/mcko.py
@@ -4,9 +4,7 @@
 def n11() -> int:
     RADIX: list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B"]
     for i in RADIX:
-        num1, num2 = int("1" + "5" + "4" + i + "3", 12), int(
-            "1" + i + "3" + "6" + "5", 12
-        )
+        num1, num2 = int(f"154{i}3", 12), int(f"1{i}365", 12)
         if (num1 + num2) % 13 == 0:
             break
     return (num1 + num2) / 13
