cf352549e01f824ca255f85c391cc9392958ca4e
ognevnydemon <maksapple2306@gmail.com>
2023-05-05 09:51:49 +0300

    try solve tasks from Russian exam

--- rusted/ognlib/src/lib.rs
+++ rusted/ognlib/src/lib.rs
@@ -1,6 +1,6 @@
 //! This library was made not for uploading to crates.io, but for personal use (for me at least).
-//! This code is not intended to be very serious. 
-//! I know, there is maybe some crates that have more useful implementations that mine. 
+//! This code is not intended to be very serious.
+//! I know, there is maybe some crates that have more useful implementations that mine.
 //! This code is made to practice Rust at something
 
 pub mod num {
@@ -13,3 +13,5 @@ pub mod algorithm {
     pub mod extra;
     pub mod sort;
 }
+
+pub mod mcko;
