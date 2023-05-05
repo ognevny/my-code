46d1f3c439b9baa8b8670226d83571663d68aace
ognevnydemon <maksapple2306@gmail.com>
2023-05-05 10:56:51 +0300

    fix mcko.py

--- pie/mcko.py
+++ pie/mcko.py
@@ -3,9 +3,9 @@
 
 def n11() -> int:
     RADIX: list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B"]
-    for i, j in enumerate(RADIX):
-        num1, num2 = int("1" + "5" + "4" + j + "3", 12), int(
-            "1" + j + "3" + "6" + "5", 12
+    for i in RADIX:
+        num1, num2 = int("1" + "5" + "4" + i + "3", 12), int(
+            "1" + i + "3" + "6" + "5", 12
         )
         if (num1 + num2) % 13 == 0:
             break
