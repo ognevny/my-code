7c00ec729230d54ba38f8010ab159017a14f1574
ognevnydemon <maksapple2306@gmail.com>
2023-05-15 08:26:07 +0300

    ognlib::num::digit small improvement

--- rusted/ognlib/src/num/digit.rs
+++ rusted/ognlib/src/num/digit.rs
@@ -1,24 +1,24 @@
 //! Functions for operations with digits
 
+use std::ops::{Add, AddAssign, Div, Mul, Rem};
+
 /// Calculate sum of digits in number
 /// # Examples
 ///
 /// ```
 /// use ognlib::num::digit::digit_sum;
 ///
-/// // it fails with `the trait bound `u32: From<i32>` is not satisfied` if I don't write `as u32`
-/// assert_eq!(digit_sum(123 as u32), 6);
-/// assert_eq!(digit_sum(444 as u32), 12);
+/// assert_eq!(digit_sum(123), 6);
+/// assert_eq!(digit_sum(444), 12);
 /// ```
 
-pub fn digit_sum<N>(mut n: N) -> u32
+pub fn digit_sum<N>(mut n: N) -> N
 where
-    N: std::ops::Rem<Output = N> + std::ops::Div<Output = N> + From<u32> + Copy + PartialEq,
-    u32: From<N>,
+    N: Rem<Output = N> + Div<Output = N> + AddAssign + From<u8> + Copy + PartialEq,
 {
-    let mut sum: u32 = 0;
+    let mut sum = N::from(0);
     while n != N::from(0) {
-        sum += <N as Into<u32>>::into(n % N::from(10));
+        sum += n % N::from(10);
         n = n / N::from(10);
     }
     sum
@@ -36,7 +36,7 @@ where
 
 pub fn digit_count<N>(mut n: N) -> u16
 where
-    N: std::ops::Div<Output = N> + From<u8> + PartialEq + Copy,
+    N: Div<Output = N> + From<u8> + PartialEq + Copy,
 {
     let mut count: u16 = 0;
     while n != N::from(0) {
@@ -58,10 +58,10 @@ where
 
 pub fn rev<N>(mut n: N) -> N
 where
-    N: std::ops::Mul<Output = N>
-        + std::ops::Add<Output = N>
-        + std::ops::Rem<Output = N>
-        + std::ops::Div<Output = N>
+    N: Mul<Output = N>
+        + Add<Output = N>
+        + Rem<Output = N>
+        + Div<Output = N>
         + From<u8>
         + Copy
         + PartialEq,
@@ -86,7 +86,7 @@ where
 
 pub fn has_digit<N>(mut n: N, k: u8) -> bool
 where
-    N: std::ops::Rem<Output = N> + std::ops::Div<Output = N> + PartialEq + From<u8> + Copy,
+    N: Rem<Output = N> + Div<Output = N> + PartialEq + From<u8> + Copy,
 {
     while n != N::from(0) {
         if n % N::from(10) == N::from(k) {
