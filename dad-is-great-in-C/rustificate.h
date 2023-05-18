ec0c2dd4eb27b596b649476aeae823eb6065697e
ognevnydemon <maksapple2306@gmail.com>
2023-05-18 19:44:10 +0300

    more rustification

--- dad-is-great-in-C/rustificate.h
+++ dad-is-great-in-C/rustificate.h
@@ -2,15 +2,43 @@
 #define u16 uint16_t
 #define u32 uint32_t
 #define u64 uint64_t
+#define u128 uint128_t
 #define i8 int8_t
 #define i16 int16_t
 #define i32 int32_t
 #define i64 int64_t
+#define i128 int128_t
 #define f32 float
 #define f64 double
+#define f128 long double
 #define String string
 #define Vec vector
 #define use using
 #define push push_back
 #define read_line getline
 #define size len
+#define let auto
+
+#ifdef _USE_MATH_DEFINES
+
+#define E M_E
+#define FRAC_1_PI M_1_PI
+#define FRAC_1_SQRT_2 M_SQRT1_2
+#define FRAC_2_PI M_2_PI
+#define FRAC_2_SQRT_PI M_2_SQRTPI
+#define FRAC_PI_2 M_PI_2
+#define FRAC_PI_3 1.04719755119659774615421446109316763
+#define FRAC_PI_4 M_PI_4
+#define FRAC_PI_6 0.52359877559829887307710723054658381
+#define FRAC_PI_8 0.39269908169872415480783042290993786
+#define LN_2 M_LN2
+#define LN_10 M_LN10
+#define LOG2_10 3.32192809488736234787031942948939018
+#define LOG2_E M_LOG2E
+#define LOG10_2 0.301029995663981195213738894724493027
+#define LOG10_E M_LOG10E
+#define PI M_PI
+#define SQRT2 M_SQRT2
+#define TAU 6.28318530717958647692528676655900577
+
+#endif
