ef8772b2f0365abe8fb785a222e371a379298943
ognevnydemon <maksapple2306@gmail.com>
2023-05-19 17:14:12 +0300

    enum PrimeStatus

--- rusted/ognlib/src/algorithm/prime.rs
+++ rusted/ognlib/src/algorithm/prime.rs
@@ -1,19 +1,28 @@
+pub enum PrimeStatus {
+    Prime,
+    NotPrime,
+    ProbablyPrime,
+}
+
 /// Wilson's theory.
 /// From [Wikipedia](https://en.wikipedia.org/wiki/Wilson%27s_theorem):
 /// "Wilson's theorem states that a natural number n > 1 is a prime
 /// number if and only if the product of all the positive integers
 /// less than n is one less than a multiple of n. That is the factorial
 /// (n - 1)! satisfies (n - 1)! % n == -1."
-/// 
+///
 /// # Examples
 /// ```
-/// use ognlib::algorithm::prime::wilson_th;
-/// 
-/// assert_eq!(wilson_th(13), true);
-/// assert_eq!(wilson_th(444), false);
+/// use ognlib::algorithm::prime::*;
+///
+/// // PrimeStatus::Prime
+/// wilson_th(13);
+///
+/// // PrimeStatus::NotPrime
+/// wilson_th(444);
 /// ```
 
-pub fn wilson_th(n: usize) -> bool {
+pub fn wilson_th(n: usize) -> PrimeStatus {
     use num_bigint::BigInt;
 
     fn factorial(n: usize) -> BigInt {
@@ -23,5 +32,9 @@ pub fn wilson_th(n: usize) -> bool {
             BigInt::from(n) * factorial(n - 1)
         }
     }
-    (factorial(n - 1) % BigInt::from(n)) - BigInt::from(n) == BigInt::from(-1)
+    if (factorial(n - 1) % BigInt::from(n)) - BigInt::from(n) == BigInt::from(-1) {
+        PrimeStatus::Prime
+    } else {
+        PrimeStatus::NotPrime
+    }
 }
