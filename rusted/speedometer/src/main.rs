8f7ed91e982965333b4c8dd21aab8e76a049c3d2
ognevnydemon <maksapple2306@gmail.com>
2023-06-12 17:51:25 +0300

    some warns

--- rusted/speedometer/src/main.rs
+++ rusted/speedometer/src/main.rs
@@ -2,12 +2,13 @@
 
 fn s() -> u32 {
     let mut n = 1;
-    while n < 1_000_000_000 {
+    while (n < 1_000_000_000) {
         n += 1
     }
     n
 }
 
 fn main() {
+    let i = 1;
     print!("{}", s());
 }
