04fda8c546f3c1c077aa45c3321a45081e94c8a1
ognevnydemon <maksapple2306@gmail.com>
2023-09-21 13:20:49 +0300

    change bubble.cpp for teacher's task

--- dad-is-great-in-C/b_sort.cpp
+++ dad-is-great-in-C/b_sort.cpp
@@ -1,36 +1,39 @@
-// bubble sort in C++
+// bubble sort
 
 #include <ctime>
 #include <iostream>
 #include <random>
 using namespace std;
 
-int main() {
-  int n;
-  cin >> n;
-  clock_t st = clock();
-  int a[n];
-  srand(time(NULL));
-  for (int i = 0; i < n; i++)
-    a[i] = rand();
-
+void bubble(int *arr, int n) {
   for (int i = n - 1; i > 0; i--) {
     int count = 0;
     for (int j = 0; j < i; j++) {
-      if (a[j] > a[j + 1]) {
-        // swap(a[j], a[j + 1]); // alt variant
-        a[j] = 2 * a[j + 1] - a[j];
-        a[j + 1] = 2 * a[j + 1] - a[j];
-        a[j] = (a[j] + a[j + 1]) / 2;
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
         count++;
       }
     }
     if (count == 0)
       break;
   }
+}
 
-  // for (int i : a) cout << i << " ";
-  clock_t end = clock();
-  cout << endl << (double)(end - st) / CLOCKS_PER_SEC;
+int main() {
+  int n;
+  cin >> n;
+  // clock_t st = clock();
+  int a[n];
+  srand(time(NULL));
+  for (int i = 0; i < n; i++) {
+    a[i] = rand();
+    cout << a[i] << " ";
+  }
+  cout << endl;
+  bubble(a, n);
+  for (int i : a)
+    cout << i << " ";
+  // clock_t end = clock();
+  // cout << endl << (double)(end - st) / CLOCKS_PER_SEC;
   return 0;
 }
