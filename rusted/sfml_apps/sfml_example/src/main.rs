9e431506f9e99682d8d6e220fa77fdb07eb549fa
ognevnydemon <maksapple2306@gmail.com>
2023-10-29 09:48:31 +0300

    rusted: add first sfml application more coming soon

---
+++ rusted/sfml_apps/sfml_example/src/main.rs
@@ -0,0 +1,43 @@
+use sfml::{
+    graphics::{
+        CircleShape, Color, FloatRect, RenderTarget, RenderWindow, Shape, Transformable, View,
+    },
+    system::Vector2f,
+    window::{Event, Key, Style},
+};
+
+fn main() {
+    let mut window = RenderWindow::new(
+        (200, 200),
+        "blazingly fast, memory safe sfml window",
+        Style::RESIZE | Style::CLOSE,
+        &Default::default(),
+    );
+    let mut shape = CircleShape::new(100., 30);
+    shape.set_fill_color(Color::GREEN);
+
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            match event {
+                Event::Closed
+                | Event::KeyPressed {
+                    code: Key::Escape, ..
+                } => window.close(),
+                Event::Resized { width, height } => {
+                    let view = View::from_rect(FloatRect::new(0., 0., width as f32, height as f32));
+                    let radius = width.min(height) as f32 / 2.;
+                    shape.set_radius(radius);
+                    shape.set_position(Vector2f::new(
+                        width as f32 / 2. - radius,
+                        height as f32 / 2. - radius,
+                    ));
+                    window.set_view(&view);
+                },
+                _ => (),
+            }
+        }
+        window.clear(Color::rgb(0, 0, 0));
+        window.draw(&shape);
+        window.display();
+    }
+}
