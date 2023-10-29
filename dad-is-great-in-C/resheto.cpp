0377bac14d0389e725068add69375b42b3770554
ognevnydemon <maksapple2306@gmail.com>
2023-10-29 15:08:12 +0300

    resheto.cpp: remove useless condition

--- dad-is-great-in-C/resheto.cpp
+++ dad-is-great-in-C/resheto.cpp
@@ -7,7 +7,7 @@ int main() {
   int n;
   cin >> n;
   bool printed = false;
-  for (int i = 2; i <= n and n > 1; i++) {
+  for (int i = 2; i <= n; i++) {
     int count = 0;
     if (n % i == 0) {
       while (n % i == 0) {
