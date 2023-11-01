1a71fc9ab7717c69a22d972099110c020217df81
ognevnydemon <maksapple2306@gmail.com>
2023-11-01 09:52:03 +0300

    rusted/integral: optimizations

--- rusted/integral/src/main.rs
+++ rusted/integral/src/main.rs
@@ -5,7 +5,10 @@ use {
     scan_fmt::{scan_fmt, scanln_fmt},
     std::{
         io::{self, Write},
-        sync::mpsc::{self, SendError},
+        sync::{
+            mpsc::{self, SendError},
+            Arc,
+        },
         thread,
     },
 };
@@ -13,7 +16,7 @@ use {
 fn f(x: f64, expr: &str) -> f64 {
     let mut context = Context::new();
     context.var("x", x);
-    eval_str_with_context(expr, &context).unwrap()
+    eval_str_with_context(expr, &context).expect("wrong variable!")
 }
 
 fn lrect(a: f64, b: f64, n: f64, expr: &str) -> f64 {
@@ -49,25 +52,28 @@ fn trapezoid(a: f64, b: f64, n: f64, expr: &str) -> f64 {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("first, write lower bound, upper bound, then epsilon in this format\n[a;b],e");
+    println!(
+        "first, write lower bound, upper bound, then epsilon respectively in this format\n[a;b],e"
+    );
     let (a, b, eps) = scanln_fmt!("[{};{}],{}", f64, f64, f64)?;
 
     println!("then write your function with `x` variable");
-    let mut exprl = String::new();
-    io::stdin().read_line(&mut exprl)?;
+    let mut expr = String::new();
+    io::stdin().read_line(&mut expr)?;
 
     let (txl, rx) = mpsc::channel();
     let (txr, txm, txt) = (txl.clone(), txl.clone(), txl.clone());
 
-    let (exprr, exprm, exprt) = (exprl.clone(), exprl.clone(), exprl.clone());
+    let expr = Arc::new(expr);
+    let (exprr, exprm, exprt) = (Arc::clone(&expr), Arc::clone(&expr), Arc::clone(&expr));
 
     thread::spawn(move || -> Result<(), SendError<_>> {
-        let (mut n, mut s1, mut s2) = (1.0, 0.0, f(a, &exprl) * (b - a));
+        let (mut n, mut s1, mut s2) = (1.0, 0.0, f(a, &expr) * (b - a));
         while (s2 - s1).abs() > eps {
             n *= 2.0;
-            (s1, s2) = (s2, lrect(a, b, n, &exprl));
+            (s1, s2) = (s2, lrect(a, b, n, &expr));
         }
-        txl.send(format!("left: {s2}"))?;
+        txl.send(format!("left: {s2}\n"))?;
         Ok(())
     });
 
@@ -77,7 +83,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             n *= 2.0;
             (s1, s2) = (s2, rrect(a, b, n, &exprr));
         }
-        txr.send(format!("right: {s2}"))?;
+        txr.send(format!("right: {s2}\n"))?;
         Ok(())
     });
 
@@ -87,7 +93,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             n *= 2.0;
             (s1, s2) = (s2, mrect(a, b, n, &exprm));
         }
-        txm.send(format!("middle: {s2}"))?;
+        txm.send(format!("middle: {s2}\n"))?;
         Ok(())
     });
 
@@ -97,14 +103,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             n *= 2.0;
             (s1, s2) = (s2, trapezoid(a, b, n, &exprt));
         }
-        txt.send(format!("trapezoid: {s2}"))?;
+        txt.send(format!("trapezoid: {s2}\n"))?;
         Ok(())
     });
 
     let mut handle = io::BufWriter::new(io::stdout());
-    writeln!(handle)?;
+    handle.write_all(b"\n")?;
     for res in rx {
-        writeln!(handle, "{res}")?;
+        handle.write_all(res.as_bytes())?;
     }
     handle.flush()?;
     Ok(())
