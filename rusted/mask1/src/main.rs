296a30d255fe649aa6d1ec8b72aacd96560f78d5
ognevnydemon <maksapple2306@gmail.com>
2023-11-03 13:09:00 +0300

    python + fnmatch? no, rust + regex, hehehe!

---
+++ rusted/mask1/src/main.rs
@@ -0,0 +1,16 @@
+use {
+    regex::Regex,
+    std::io::{self, BufWriter, Write},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mask = Regex::new(r"^123.*567.?$")?;
+    let mut handle = BufWriter::new(io::stdout());
+    for num in (169..10000000000usize).step_by(169) {
+        if mask.is_match(&num.to_string()) {
+            writeln!(handle, "{num} {}", num / 169)?;
+        }
+    }
+    handle.flush()?;
+    Ok(())
+}
