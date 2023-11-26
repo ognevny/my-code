86bcacf91b454df0028646fbc8bd1befb02b645d
ognevnydemon <maksapple2306@gmail.com>
2023-11-26 14:16:27 +0300

    rusted/resheto: refactor

--- rusted/resheto/src/main.rs
+++ rusted/resheto/src/main.rs
@@ -1,32 +1,55 @@
-#![allow(clippy::mut_range_bound)]
 use std::io::{self, Write};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut n = String::new();
     let mut handle = io::BufWriter::new(io::stdout());
     io::stdin().read_line(&mut n)?;
-    let (mut n, mut printed) = (n.trim().parse()?, false);
-    for i in 2..=n {
-        let mut count = 0;
-        if n % i == 0 {
-            while n % i == 0 {
-                n /= i;
-                count += 1;
-            }
-            if printed {
-                if count == 1 {
+    let (mut n, mut printed): (u64, bool) = (n.trim().parse()?, false);
+    let (mut count, m) = (0, n);
+    while n % 2 == 0 {
+        n /= 2;
+        count += 1;
+    }
+    match count {
+        1 => {
+            write!(handle, "2")?;
+            printed = true;
+        },
+        2.. => {
+            write!(handle, "2^{count}")?;
+            printed = true
+        },
+        _ => (),
+    }
+    for i in (3..).step_by(2).take_while(|i| i * i <= m) {
+        count = 0;
+        while n % i == 0 {
+            n /= i;
+            count += 1;
+        }
+        match count {
+            1 =>
+                if printed {
                     write!(handle, "*{i}")?;
                 } else {
-                    write!(handle, "*{i}^{count}")?;
-                }
-            } else {
-                if count == 1 {
                     write!(handle, "{i}")?;
+                    printed = true;
+                },
+            2.. =>
+                if printed {
+                    write!(handle, "*{i}^{count}")?;
                 } else {
                     write!(handle, "{i}^{count}")?;
-                }
-                printed = true;
-            }
+                    printed = true;
+                },
+            _ => (),
+        }
+    }
+    if n > 2 {
+        if printed {
+            write!(handle, "*{n}")?;
+        } else {
+            write!(handle, "{n}")?;
         }
     }
     handle.flush()?;
