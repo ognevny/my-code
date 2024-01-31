e4b060ac7df0887f0612779ea2a25444261338f6
ognevnydemon <maksapple2306@gmail.com>
2024-01-31 11:39:54 +0300

    first exam task

---
+++ rusted/ege1/src/main.rs
@@ -0,0 +1,29 @@
+//! the first code with tasks from EGE examW
+
+use {
+    ansi_term::Colour,
+    std::{cmp::max, fs},
+};
+
+fn ex1() -> Result<usize, Box<dyn std::error::Error>> {
+    let text = fs::read_to_string("11.txt")?;
+    let newtext = text.replace("INFINITY", "@");
+    let mut pos = Vec::new();
+    for (i, char) in newtext.chars().enumerate() {
+        if char == '@' {
+            pos.push(i);
+        }
+    }
+    let mut mx = 0;
+    for i in 0..(pos.len() - 1001) {
+        mx = max(pos[i + 1001] - pos[i] - 1 + 7000 + 14, mx);
+    }
+    Ok(mx)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ex1 = ex1()?;
+    assert_eq!(ex1, 36747);
+    println!("{}", Colour::Green.bold().paint(ex1.to_string()));
+    Ok(())
+}
